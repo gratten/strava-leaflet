0f426adb873650fe057670d8b0cb7d81fb35d691
gratten <grattenward@gmail.com>
2023-08-14 22:13:55 -0400

    added miles to popup and dark basemap

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ interface Activity {
   activityName: string;
   activityElevation: number;
   activityDate: string;
+  activityDistance: string;
 }
 
 function App() {
@@ -41,19 +42,21 @@ function App() {
       let page = 1;
       while (true) {
         const stravaActivities = await fetchActivities(page);
-        // console.log(stravaActivities)
+        console.log(stravaActivities)
         if (stravaActivities.length > 0) {
           for (const stravaActivity of stravaActivities) {
             const activity_polyline = stravaActivity.map.summary_polyline;
             const activity_name = stravaActivity.name;
             const activity_elevation = stravaActivity.total_elevation_gain;
             const activity_date = stravaActivity.start_date;
+            const activity_distance = (stravaActivity.distance/1609).toFixed(2) + " miles"
             const decodedPolyline = polyline.decode(activity_polyline);
             allActivities.push({
               activityPositions: decodedPolyline,
               activityName: activity_name,
               activityElevation: activity_elevation,
-              activityDate: activity_date
+              activityDate: activity_date,
+              activityDistance: activity_distance
             });
           }
           page++;
@@ -81,14 +84,16 @@ function App() {
       ) : (
       <MapContainer center={[33.77196, -84.38370]} zoom={12} scrollWheelZoom={true} style={{ width: '100%', height: '100vh' }}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          // url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+          url = "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
         {activities.map((activity, i) => (
           <Polyline key={i} positions={activity.activityPositions}>
             <Popup>
               <div>
                 <h2>{activity.activityName}</h2>
+                <p>{activity.activityDistance}</p>
                 <p>{activity.activityDate}</p>
               </div>
             </Popup>
